27c8859db9fbf0600853ae920a153633d0b4235e
Ryan Ishimaru <rishimaru23@gmail.com>
2021-07-13 15:16:15 -0700

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,23 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 17). notNullable().unique()
+
+    tbl.string('make', 128)
+
+    tbl.string('model', 128).notNullable()
+
+    tbl.numeric('mileage').unsigned().notNullable()
+
+    tbl.string('title', 128)
+
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('cars')
   // DO YOUR MAGIC
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars  = [
+    {
+        vin: '111111111111',
+        make: "toyota",
+        model: 'prius',
+        milege: 215000,
+        title: 'clean',
+        transmission:'manuel'
+    },
+    {
+        vin: '111111111111',
+        make: "toyota",
+        model: 'corolla',
+        milege: 115000,
+        title: 'salvage',
+    },
+    {
+        vin: '111111111111',
+        make: "ford",
+        model: 'focus',
+        milege: 15000,
+    },
+]
+
+exports.seed = function(knex){
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('knex').insert(cars)
+    })
+    
+}
