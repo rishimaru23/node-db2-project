2c2b5227be27601bddad0716c31d36b77b7b85d2
Ryan Ishimaru <rishimaru23@gmail.com>
2021-07-13 17:10:08 -0700

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,53 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Car = require('./cars-model')
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Car.getById(req.params.id)
+    if(!car){
+      next({ status:404, message:'not found'})
+    } else {
+      req.car = car
+      next()
+    }
+} catch(err){
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  if(!req.body.vin) return next({
+    status:400,
+    message:'vin is missing',
+  })
+  if(!req.body.make) return next({
+    status:400,
+    message:'make is missing',
+  })
+  if(!req.body.model) return next({
+    status:400,
+    message:'model is missing',
+  })
+  if(!req.body.mileage) return next({
+    status:400,
+    message:'mileages is missing',
+  })
+  next()
 }
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
+  next()
 }
 
 const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR 
+  next()
+}
+
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
